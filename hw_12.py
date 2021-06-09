a7ec08ae9ed2660c890507b738a94de07a932805
jjennyko <71782368+jjennyko@users.noreply.github.com>
2021-06-09 16:30:34 +0800

    Add files via upload

---
+++ hw_12.py
@@ -0,0 +1,20 @@
+#1.
+import pandas as pd
+import numpy as np
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data, index=labels)
+print(df.loc[['d','e','i'],('animal','age')])
+
+#2.
+
+df = pd.DataFrame([
+    ['tom', 'mark', 'mary'],
+    ['bob', 'alice', 'john']
+])
+print(df.applymap(lambda i : str(i).title()))
